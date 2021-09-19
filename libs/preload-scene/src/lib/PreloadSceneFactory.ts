e44c193944ab6540d3a401aeae2ebeeb648476ed
Jose Francisco Brand Thomazini <jose.thomazini@gmail.com>
2021-09-19 00:06:07 -0300

    multiple webfonts

--- libs/preload-scene/src/lib/PreloadSceneFactory.ts
+++ libs/preload-scene/src/lib/PreloadSceneFactory.ts
@@ -1,12 +1,9 @@
-import getLogger from '@games/logger';
 import { createSoundManager } from '@games/sound-manager';
 import Phaser from 'phaser';
 import { Globals } from '..';
 import EventHandler from './EventHandler';
 import { Asset, loadAssetsByType } from './EventHandler/AssetsLoader';
 import WebFontFile from './WebFontLoader';
-
-const logger = getLogger('PreloadSceneFactory')
 
 
 export default function PreloadSceneFactory(data: any, postScene: string) {
@@ -16,16 +13,13 @@ export default function PreloadSceneFactory(data: any, postScene: string) {
     }
 
     preload() {
-      logger.info('info')
       new EventHandler(this);
 
       loadAssetsByType(this, data, Asset.images);
       loadAssetsByType(this, data, Asset.audio);
       loadAssetsByType(this, data, Asset.sprites);
 
-      // TODO: list of fonts
-      this.load.addFile(new WebFontFile(this.load, data.webFont))
-
+      this.load.addFile(new WebFontFile(this.load, data.webfonts))
     }
 
     create() {
