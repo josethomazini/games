a3650ba3f65f9b85b92d6b2d4216631b40171b60
Jose Francisco Brand Thomazini <jose.thomazini@gmail.com>
2021-09-18 23:01:21 -0300

    make printmousecoords to work only on dev

--- libs/print-mouse-coords/src/lib/PrintMouseCoords.ts
+++ libs/print-mouse-coords/src/lib/PrintMouseCoords.ts
@@ -4,15 +4,23 @@ export default class PrintMouseCoords {
   style: Phaser.Types.GameObjects.Text.TextStyle
   text: Phaser.GameObjects.Text
   scene: Phaser.Scene
+  color: string
 
   constructor(scene: Phaser.Scene, color = '#00ff00') {
     this.scene = scene;
+    this.color = color;
+  }
 
-    this.style = <Phaser.Types.GameObjects.Text.TextStyle>{ fill: color };
+  create() {
+    if(process.env.NODE_ENV === 'production') return;
+
+    this.style = <Phaser.Types.GameObjects.Text.TextStyle>{ fill: this.color };
     this.text = this.scene.add.text(10, 10, '', this.style).setDepth(1);
   }
 
   update() {
+    if(process.env.NODE_ENV === 'production') return;
+
     const pointer = this.scene.input.activePointer;
 
     this.text.setText([
