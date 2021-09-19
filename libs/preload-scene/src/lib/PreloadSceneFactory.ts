3b5d4d56d4fb867d4a14b5eddbd938041888df44
Jose Francisco Brand Thomazini <jose.thomazini@gmail.com>
2021-09-18 23:02:50 -0300

    preloads stars the soundmanager

--- libs/preload-scene/src/lib/PreloadSceneFactory.ts
+++ libs/preload-scene/src/lib/PreloadSceneFactory.ts
@@ -1,25 +1,37 @@
+import getLogger from '@games/logger';
+import { createSoundManager } from '@games/sound-manager';
 import Phaser from 'phaser';
+import { Globals } from '..';
 import EventHandler from './EventHandler';
 import { Asset, loadAssetsByType } from './EventHandler/AssetsLoader';
 import WebFontFile from './WebFontLoader';
 
+const logger = getLogger('PreloadSceneFactory')
+
+
 export default function PreloadSceneFactory(data: any, postScene: string) {
   return class PreloadScene extends Phaser.Scene {
     constructor() {
-      super('PRELOAD_SCENE');
+      super('preload-scene');
     }
 
     preload() {
+      logger.info('info')
       new EventHandler(this);
 
       loadAssetsByType(this, data, Asset.images);
       loadAssetsByType(this, data, Asset.audio);
       loadAssetsByType(this, data, Asset.sprites);
 
-      this.load.addFile(new WebFontFile(this.load, 'Press Start 2P'))
+      // TODO: list of fonts
+      this.load.addFile(new WebFontFile(this.load, data.webFont))
+
     }
 
     create() {
+      Globals['soundManager'] = createSoundManager(this);
+
+
       this.scene.start(postScene);
     }
   };
