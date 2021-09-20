e02ab2110ea6146bee99f64a49fa2b7c763278a9
Jose Francisco Brand Thomazini <jose.thomazini@gmail.com>
2021-09-19 23:16:09 -0300

    show option for the print coords

--- libs/print-mouse-coords/src/lib/PrintMouseCoords.ts
+++ libs/print-mouse-coords/src/lib/PrintMouseCoords.ts
@@ -5,27 +5,44 @@ export default class PrintMouseCoords {
   text: Phaser.GameObjects.Text
   scene: Phaser.Scene
   color: string
+  configs: any
+  show: boolean
 
-  constructor(scene: Phaser.Scene, color = '#00ff00') {
+  constructor(scene: Phaser.Scene, color = '#00ff00', show = true) {
     this.scene = scene;
     this.color = color;
+    this.show = process.env.NODE_ENV !== 'production' && show
+
+    this.configs = {
+      xPos: 10,
+      yPos: 10,
+      color,
+      initialValue: '',
+      style: <Phaser.Types.GameObjects.Text.TextStyle>{ fill: this.color }
+    }
+
+    this.create()
   }
 
   create() {
-    if(process.env.NODE_ENV === 'production') return;
+    if(!this.show) return;
 
-    this.style = <Phaser.Types.GameObjects.Text.TextStyle>{ fill: this.color };
-    this.text = this.scene.add.text(10, 10, '', this.style).setDepth(1);
+    this.text = this.scene.add.text(
+      this.configs.xPos,
+      this.configs.yPos,
+      this.configs.initialValue,
+      this.configs.style
+    ).setDepth(1);
   }
 
   update() {
-    if(process.env.NODE_ENV === 'production') return;
+    if(!this.show) return;
 
     const pointer = this.scene.input.activePointer;
 
     this.text.setText([
-        'x: ' + pointer.worldX,
-        'y: ' + pointer.worldY
+      'x: ' + pointer.worldX,
+      'y: ' + pointer.worldY
     ]);
   }
 }
